e99eb12e4238af86de76c62c5fc6085a6eca98de
Herljos Scheindorf <herljos@scheindorf.info>
2018-12-10 22:26:07 +0100

    Heko for users, help for installing modules

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # berrymedBluetoothOxymeter
 Data acquisition and GUI for BerryMed Oxymeter
+
+
+DISCLAIMER
+This program was wrote for my specific device, I don't know bluetooth well and I don't know if the device id of my
+oxymeter is the same as every other produced devices, or if each one has unique id.
+If your device is a berrymed oxymeter and your run is telling that there are no host, then :
+run bluetoothctl
+enter : power on
+then : scan on
+make sure you had turn on your oxymeter at this point
+then enter : devices
+and select the hexadecimal part of the line "Device 8C:DE:52:65:12:4F BerryMed"
+With this key, open parser/sp02.py and at line 566 replace the hexa id here by yours : bd_addr = "8C:DE:52:65:12:4F"
+
+Please read DEPENDS.txt for informations about required things.
+
+making it short: 
+bluetooth is read and converted to structured JSON by the parser. Find it in parser directory.
+Then you can pipe this JSON to a file, or better, to the stdin of the GUI interface. It's in gui dir.
+There are multiple files cuz I tested with blocking IO and non blocking IO.
+
+You can also replay JSON from a saved file by cating it piped in gui.
+ But this can lead to a bug because of the speed the datas are received this way.
+So you can use ratelimit.py in parser directory.
+
+
+=== Read and View ===
+python2.7 berrymedBluetoothOxymeter/parser/sp02.py | python2.7 berrymedBluetoothOxymeter/gui/jsonread.nonblock.py
+
+=== Read, Record ===
+python2.7 berrymedBluetoothOxymeter/parser/sp02.py > /path/to/file.json 
+
+=== Read, Record, View ===
+python2.7 berrymedBluetoothOxymeter/parser/sp02.py | tee  /path/to/file.json |  python2.7 berrymedBluetoothOxymeter/gui/jsonread.nonblock.py
+
+=== Replay a file ===
+cat /path/to/json | python2.7 berrymedBluetoothOxymeter/parser/ratelimit.py |  python2.7 berrymedBluetoothOxymeter/gui/jsonread.nonblock.py
+
+
+===Experimental stuff in gui===
+add "--experiments" option :  python2.7 berrymedBluetoothOxymeter/gui/jsonread.nonblock.py --experiments
---
+++ fetchmodules.py
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+PIPEXE="pip2"
+PYTHONEXE="/usr/bin/python2.7"
+DEVICEID="8C:DE:52:65:12:4F"
+
+ARGLIST=" ./fetchmodules.py '<PIP2 Path>' '<python2 path>' '<your bluetooth device id>"
+ARGNUM=3
+
+IDPATCH="/tmp/id.patch"
+
+if [[ -e $(pwd)/parser ]] && [[ -e $(pwd)/gui ]]
+then
+
+
+if [[ -n "$1" ]]
+then if [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]
+    then echo "this script ensure to install all required modules using pip for python 2"
+         echo "it can also patch the original code to adapt to your device and preferences"
+         echo "==="
+         echo "Usage: " $ARGLIST " ;"
+         echo "You can run the script with all defaults params by running without arguments"
+         echo "Or you can run it customized and then you need to supply ALL the arguyments"
+         exit 1
+   fi
+   if [[ $# -lt $ARGNUM ]]
+   then echo "You need to provide ALL the arguments : " $ARGLIST
+        exit 2
+   fi
+   PIPEXE="$1"
+   PYTHONEXE="$2"
+   DEVICEID="$3"
+fi
+
+echo "Using $PIPEXE to install modules"
+echo "Using python exec at $PYTHONEXE"
+echo "Patch parser with device id : $DEVICEID"
+
+sleep 1
+
+echo "= require bluetooth module ="
+
+sleep 1
+
+echo $PIPEXE "install pybluez"
+$PIPEXE install pybluez
+
+sleep 1
+echo "= Patching device ID ="
+sleep 1
+
+echo "--- parser/sp02.py      2017-04-18 09:01:28.542629549 +0200" > "$IDPATCH"
+echo "+++ /tmp/spo.pt 2018-12-10 21:47:41.217490514 +0100" >> "$IDPATCH"
+echo "@@ -563,7 +563,7 @@" >> "$IDPATCH"
+echo "                        dsrdtr=False," >> "$IDPATCH"
+echo "                        interCharTimeout=None)" >> "$IDPATCH"
+echo "     rf.open() \"\"\"" >> "$IDPATCH"
+echo "-    bd_addr = \"8C:DE:52:65:12:4F\"" >> "$IDPATCH"
+echo "+    bd_addr = \"$DEVICEID\"" >> "$IDPATCH"
+echo "     port = 6" >> "$IDPATCH"
+echo "     sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )" >> "$IDPATCH"
+echo "     sock.connect((bd_addr, port))" >> "$IDPATCH"
+cp -v parser/sp02.py parser/sp02.origine.py
+if [[ -e parser/sp02.origine.py ]]
+then "copied original file as parser/sp02.origine.py , now apply patch $IDPATCH on parser/sp02.py"
+     sleep 1
+     patch  parser/sp02.py "$IDPATCH" && echo "Backuped then patched sp02.py" || { echo "patching failed, critical error" ; exit 4 ; }
+else echo "can't make a copy of sp02.py, I wont patch it, halting"
+     exit 5
+fi
+echo " === "
+echo " Everything should be working now, if nothing failed. "
+echo " Kenore tawa "
+echo " ____________________________________________________ "
+exit 0
+else echo "please run this script from the project root path"
+     exit 3
+fi
